dbe3d700b4dfbcfbaf8664639ea7b9794df55c54
Wendy Wang <wangtoro0809@gmail.com>
2019-03-19 00:40:50 +0800

    second_version

--- psw_test.py
+++ psw_test.py
@@ -1,13 +1,10 @@
 i=3
 password = 'a123456'
-while True:
+while i != 0:
 	psw = input('請輸入密碼: ')
 	if psw == password:
 		print('登入成功')
 		break;
 	else:
 		i = int(i) - 1
-		if int(i) == 0:
-			break;
-		else:
-			print('密碼錯誤! 還有', i, '次機會')
+		print('密碼錯誤! 還有', i, '次機會')
